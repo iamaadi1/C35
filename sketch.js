dc1bd3e70796e9670d63e03c9c654b04ad0d1644
iamaadi1 <77841045+iamaadi1@users.noreply.github.com>
2021-10-18 20:12:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var aball;
+var database;
+var position;
+
+function setup(){
+    database = firebase.database()
+    console.log(database)
+    createCanvas(500,500);
+
+    aball = createSprite(250,250,10,10);
+    aball.shapeColor = "red";
+
+    var aballposition = database.ref('ball/position');
+    aballposition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(position !==undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}}
+function readPosition(data){
+position = data.val();
+
+aball.x = position.x;
+aball.y = position.y
+
+    
+}
+function showError(){
+    console.log("Data not received")
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+'x':position.x+x,
+'y':position.y+y,
+
+
+        
+    })
+   
+}
